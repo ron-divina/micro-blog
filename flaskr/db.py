31359bd71d51ebfa598110d65a2eb50cbf98af26
Ron Divina <rondavin.divina@gmail.com>
2020-04-25 16:52:37 +0800

    Add auth blueprint

--- flaskr/db.py
+++ flaskr/db.py
@@ -8,7 +8,7 @@ from flask.cli import with_appcontext
 def get_db():
 	if 'db' not in g:
 		g.db = sqlite3.connect(
-			current_app.config['DATABASE']
+			current_app.config['DATABASE'],
 			detect_types=sqlite3.PARSE_DECLTYPES
 		)
 		g.db.row_factory = sqlite3.Row
@@ -24,7 +24,7 @@ def close_db(e=None):
 def init_db():
 	db = get_db()
 
-	with current_app.open_resource(schema.sql) as f:
+	with current_app.open_resource('schema.sql') as f:
 		db.executescript(f.read().decode('utf8'))
 
 @click.command('init-db')
